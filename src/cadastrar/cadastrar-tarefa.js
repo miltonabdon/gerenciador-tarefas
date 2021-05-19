0591f9e3e673f8014ea20425236fde2e9596a51c
Milton <miltonabdon69@gmail.com>
2021-05-18 22:51:57 -0300

    criacao formulario cadastro tarefas

--- src/cadastrar/cadastrar-tarefa.js
+++ src/cadastrar/cadastrar-tarefa.js
@@ -1,7 +1,48 @@
+import { A } from "hookrouter";
 import React from "react";
+import { Button, Form, Jumbotron, Modal } from "react-bootstrap";
 
 function CadastrarTarefa() {
-  return <h1>Cadastrar Tarefa</h1>;
+  return (
+    <div>
+      <h3 className="text-center">Cadastrar</h3>
+      <Jumbotron>
+        <Form>
+          <Form.Group>
+            <Form.Label>Tarefa</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Digite a tarefa"
+              minLength="5"
+              maxLength="100"
+              required
+            />
+            <Form.Control.Feedback type="invalid">
+              A tarefa deve conter ao menos 5 caracteres
+            </Form.Control.Feedback>
+          </Form.Group>
+          <Form.Group className="text-center">
+            <Button variant="success" type="submit">
+              Cadastrar
+            </Button>
+            &nbsp;
+            <A href="/" className="btn btn-light">
+              Voltar
+            </A>
+          </Form.Group>
+        </Form>
+        <Modal show={false}>
+          <Modal.Header closeButton>
+            <Modal.Title>Sucesso</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>Tarefa adicionada com sucesso!</Modal.Body>
+          <Modal.Footer>
+            <Button variant="success"> Continuar </Button>
+          </Modal.Footer>
+        </Modal>
+      </Jumbotron>
+    </div>
+  );
 }
 
 export default CadastrarTarefa;
